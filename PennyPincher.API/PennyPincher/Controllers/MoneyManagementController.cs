91546acd364ec129ef0e28645a26ec3cc3385873
Joseph-Ramon-Lucas <joseph-ramon-lucas@gmail.com>
2024-12-11 21:53:28 -0500

    initial money management controller

---
+++ PennyPincher.API/PennyPincher/Controllers/MoneyManagementController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PennyPincher.Controllers
+{
+    [ApiController]
+    [Route("/api/management")]
+    public class MoneyManagementController : Controller
+    {
+        private static List<int> Monies = new List<int>();
+
+        // handle:
+        // get money, set money
+        // 
+
+        [HttpGet]
+        public List<int> getMonies()
+        {
+            return Monies;
+
+        }
+
+        [HttpPost]
+        public ActionResult addMonies(int ammount)
+        {
+            Monies.Add(ammount);
+            return Ok("Added "+ammount+" of money.");
+        }
+
+    }
+}
