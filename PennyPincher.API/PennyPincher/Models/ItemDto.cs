b98f6b0937567d7ed0a486138997663ea962a7df
Eric Nguyen <53154237+EricNguyen-B@users.noreply.github.com>
2025-01-11 09:05:32 -0500

    Add CRUD functionality for managing purchasing history (#3)
    * Initial work on log history controller, recording items on budget
    
    * Initial work on log history controller, recording items on budget
    
    * define functional create endpoints for item controller
    
    * Define functional update and delete endpoints for LogHistoryController.cs
    
    * Assign integer values to enum types for effective update params
    
    * Add null for optional price field and endpoint to get items by category
    
    * Defined CashFlow Objects and API endpoints
    
    * organizing and modularizing cash flows with dtos
    
    * added dummy data for CashFlowDataStore
    
    * updated endpoints to support new cashflowdto type
    
    * supported getting specific cashflow ids
    
    * lambdafied getting a target flow
    
    * can now get specifically all expenses or all incomes
    
    * fixed up the status endpoint to work with CashFlowDto data
    
    * added way to update CFs
    
    * Added delete CashFlow endpoint
    
    * Fixed null value issue with costly amount variable
    
    * Addding restrictions and more CF Dto models
    
    * Added new Json packages to Support Patch Endpoint for CashFlows.
    Completed Cashflow Patch endpoint
    
    * Addressed PR changes. Refactored the way how put endpoint updates data.
    Standardized endpoint response messages and endpoint routes that require target ids
    
    * Simplified endpoints to use where clauses when searching for CF Ids
    
    * Better variable names
    
    * Related CFDto and CFUpdateDto via inheritance.
    Updated the Microsoft.AspNetCore.Mvc.NewtonsoftJson package to match Log Controller branch
    
    ---------
    
    Co-authored-by: Joseph-Ramon-Lucas <joseph-ramon-lucas@gmail.com>
    
    ---------
    
    Co-authored-by: Joseph-Ramon-Lucas <76121642+Joseph-Ramon-Lucas@users.noreply.github.com>
    Co-authored-by: Joseph-Ramon-Lucas <joseph-ramon-lucas@gmail.com>

---
+++ PennyPincher.API/PennyPincher/Models/ItemDto.cs
@@ -0,0 +1,26 @@
+using System.Diagnostics.CodeAnalysis;
+
+namespace PennyPincher.Models
+{
+    public enum CategoryTypes
+    {
+        None = 0,
+        Living = 1,
+        Utilities = 2, 
+        Entertainment = 3,
+        Shopping = 4,
+        Takeout = 5
+    }
+    
+    public class ItemDto
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+        
+        [AllowNull]
+        public CategoryTypes Category { get; set; }
+        
+        public double Price { get; set; }
+    }   
+}
