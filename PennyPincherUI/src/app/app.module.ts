bc9b4e54e0d39c6caa86c9fa7bb8cbf5f59cd30d
Joseph-Ramon-Lucas <joseph.ramon.lucas@gmail.com>
2025-02-26 14:38:34 -0500

    new navigation routes created

--- PennyPincherUI/src/app/app.module.ts
+++ PennyPincherUI/src/app/app.module.ts
@@ -2,15 +2,15 @@ import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { BrowserModule } from "@angular/platform-browser";
+import { RouterModule } from "@angular/router";
 
 import { AppComponent } from "./app.component";
 import { ExpenseForm } from "./expenses/expense-form.component";
 
 @NgModule({
-  imports: [BrowserModule, CommonModule, FormsModule],
-  declarations: [AppComponent, ExpenseForm],
-  providers: [],
-  bootstrap: [AppComponent]
+	imports: [BrowserModule, RouterModule, CommonModule, FormsModule],
+	declarations: [AppComponent, ExpenseForm],
+	providers: [],
+	bootstrap: [AppComponent],
 })
-
 export class AppModule {}
