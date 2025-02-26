9f31ba1e420242bbbb715682357da8ef8d3cea4c
Joseph-Ramon-Lucas <joseph.ramon.lucas@gmail.com>
2025-02-26 18:07:11 -0500

    styled navbar to look pretty nice

--- PennyPincherUI/src/app/nav-bar/nav-bar.component.ts
+++ PennyPincherUI/src/app/nav-bar/nav-bar.component.ts
@@ -1,9 +1,11 @@
 import { Component } from "@angular/core";
 import { RouterLink } from "@angular/router";
-
+import { MatIconModule } from "@angular/material/icon";
+import { MatDividerModule } from "@angular/material/divider";
+import { MatButtonModule } from "@angular/material/button";
 @Component({
 	selector: "app-nav-bar",
-	imports: [RouterLink],
+	imports: [RouterLink, MatButtonModule, MatDividerModule, MatIconModule],
 	templateUrl: "./nav-bar.component.html",
 	styleUrl: "./nav-bar.component.css",
 })
