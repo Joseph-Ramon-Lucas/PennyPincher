35e710321def1ad7fd1b308a5e7eb1c1da985e9c
Joseph-Ramon-Lucas <joseph.ramon.lucas@gmail.com>
2025-02-28 11:44:53 -0500

    Updated expense-history service endpoints

--- PennyPincherUI/src/app/services/expense-history.service.ts
+++ PennyPincherUI/src/app/services/expense-history.service.ts
@@ -2,13 +2,16 @@ import { HttpClient } from "@angular/common/http";
 import { inject, Injectable } from "@angular/core";
 import { environment } from "../../environments/environment.development";
 import type { Observable } from "rxjs";
-import type { Expense } from "../models/expense";
+import {
+	CATEGORY_TYPES,
+	Expense,
+	ExpenseForUpdateDto,
+} from "../models/expense";
 
 @Injectable({ providedIn: "root" })
 export class ExpenseHistoryService {
 	private http = inject(HttpClient);
-	private apiUrl = environment.apiURL + "/api/history";
-
+	private apiUrl = `${environment.apiURL}/api/history`;
 	constructor() {}
 
 	public addItem(itemToAdd: Expense): Observable<Expense> {
@@ -22,11 +25,29 @@ export class ExpenseHistoryService {
 		return this.http.get<Expense[]>(this.apiUrl);
 	}
 
-	public getItemById(): any {}
+	public getItemById(itemId: number): Observable<Expense[]> {
+		return this.http.get<Expense[]>(`${this.apiUrl}/${itemId}`);
+	}
 
-	public getItemByCategory(): any {}
+	public getItemByCategory(
+		categoryType: CATEGORY_TYPES,
+	): Observable<Expense[]> {
+		return this.http.get<Expense[]>(
+			`${this.apiUrl}/${categoryType}/getcategoryitems`,
+		);
+	}
 
-	public updateItem(): any {}
+	public updateItem(
+		itemId: number,
+		itemWithUpdate: ExpenseForUpdateDto,
+	): Observable<ExpenseForUpdateDto> {
+		return this.http.put<ExpenseForUpdateDto>(
+			`${this.apiUrl}/${itemId}`,
+			itemWithUpdate,
+		);
+	}
 
-	public deleteItem(): void {}
+	public deleteItem(itemToDelete: Expense): Observable<Expense> {
+		return this.http.delete<Expense>(this.apiUrl);
+	}
 }
