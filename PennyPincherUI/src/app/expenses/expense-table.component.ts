2b91e231c55ba892beb26db18a797cba9607cbaa
Joseph-Ramon-Lucas <joseph.ramon.lucas@gmail.com>
2025-02-28 01:18:51 -0500

    Added spinner to indicate loading expense table

--- PennyPincherUI/src/app/expenses/expense-table.component.ts
+++ PennyPincherUI/src/app/expenses/expense-table.component.ts
@@ -11,12 +11,19 @@ import { MatIconModule } from "@angular/material/icon";
 import { MatDividerModule } from "@angular/material/divider";
 import { MatButtonModule } from "@angular/material/button";
 import { MatTooltipModule } from "@angular/material/tooltip";
+import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
 
 @Component({
 	selector: "expense-table",
 	templateUrl: "./expense-table.component.html",
 	styleUrl: "./expense-table.component.css",
-	imports: [MatButtonModule, MatDividerModule, MatIconModule, MatTooltipModule],
+	imports: [
+		MatButtonModule,
+		MatDividerModule,
+		MatIconModule,
+		MatTooltipModule,
+		MatProgressSpinnerModule,
+	],
 })
 export class ExpenseTable implements OnInit {
 	title = "ExpenseTable";
