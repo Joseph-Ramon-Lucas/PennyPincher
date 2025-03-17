8422e8411c412efb2efe7339053e807cf25542fb
Eric Nguyen <enguyen095@gmail.com>
2025-03-16 21:35:37 -0400

    Continued work

--- PennyPincher.API/PennyPincher/Repositories/ExpenseRepository.cs
+++ PennyPincher.API/PennyPincher/Repositories/ExpenseRepository.cs
@@ -1,8 +1,55 @@
-﻿namespace PennyPincher.Repositories
+﻿using System.Data;
+using Dapper;
+using PennyPincher.Data;
+
+namespace PennyPincher.Repositories
 {
     public class ExpenseRepository : IExpenseRepository
     {
+        private readonly IDbConnection _dbConnection;
+        
+        public ExpenseRepository(IDbConnection connection)
+        {
+            _dbConnection = connection;
+        }
 
+        public async Task<IEnumerable<Expense>> GetAllExpensesAsync()
+        {
+            string sql = "SELECT id, user_id, name, category_type, price FROM expenses";
+            return await _dbConnection.QueryAsync<Expense>(sql);
+        }
 
+        public async Task<Expense?> GetStudentAsync(int id)
+        {
+            string sql = "SELECT id, user_id, name, category_type, price FROM expenses WHERE id = @id";
+            return await _dbConnection.QueryFirstOrDefaultAsync<Expense>(sql, new { id });
+        }
+
+        public async Task<int> AddExpenseAsync(Expense expense)
+        {
+            string sql = @"
+                INSERT INTO expenses (name, category_type, price) 
+                VALUES (@name, @category_type, @price)
+                SELECT LAST_INSERT_ID();";
+            
+            return await _dbConnection.ExecuteAsync(sql, expense);
+        }
+
+        public async Task<bool> UpdateExpenseAsync(Expense expense)
+        {
+            string sql = @"
+                UPDATE expenses 
+                SET name = @name, category_type = @category_type, price = @price";
+            
+            int rowsAffected = await _dbConnection.ExecuteAsync(sql, expense);
+            return rowsAffected > 0;    
+        }
+
+        public async Task<bool> DeleteExpenseAsync(int id)
+        {
+            string sql = "DELETE FROM expenses WHERE Id = @Id";
+            int rowsAffected = await _dbConnection.ExecuteAsync(sql, new { Id = id });
+            return rowsAffected > 0;
+        }
     }
 }
--- PennyPincher.API/PennyPincher/Repositories/IExpenseRepository.cs
+++ PennyPincher.API/PennyPincher/Repositories/IExpenseRepository.cs
@@ -1,6 +1,17 @@
-﻿namespace PennyPincher.Repositories
+﻿using PennyPincher.Data;
+
+namespace PennyPincher.Repositories
 {
     public interface IExpenseRepository
     {
+        Task<IEnumerable<Expense>> GetAllExpensesAsync();
+        
+        Task<Expense> GetStudentAsync(int id);
+        
+        Task<int> AddExpenseAsync(Expense expense);
+        
+        Task<bool> UpdateExpenseAsync(Expense expense);
+        
+        Task<bool> DeleteExpenseAsync(int id);
     }
 }
