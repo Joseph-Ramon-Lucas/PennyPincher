1fa77385a37b9fb6014b735637308af8ef8ea4cb
Joseph-Ramon-Lucas <joseph.ramon.lucas@gmail.com>
2025-03-26 18:51:27 -0400

    Converting over cashflow table to use reactive forms

--- PennyPincherUI/src/app/cash-flows/cash-flow-forms/add-cash-flow-panel/add-cash-flow-panel.component.ts
+++ PennyPincherUI/src/app/cash-flows/cash-flow-forms/add-cash-flow-panel/add-cash-flow-panel.component.ts
@@ -8,7 +8,12 @@ import {
 	Output,
 	SimpleChanges,
 } from "@angular/core";
-import { FormsModule } from "@angular/forms";
+import {
+	FormControl,
+	FormGroup,
+	FormsModule,
+	ReactiveFormsModule,
+} from "@angular/forms";
 import { MatButtonModule } from "@angular/material/button";
 import { MatCardModule } from "@angular/material/card";
 import { MatDividerModule } from "@angular/material/divider";
@@ -20,7 +25,8 @@ import { MatTabsModule } from "@angular/material/tabs";
 import { MatTooltipModule } from "@angular/material/tooltip";
 import { MatRadioModule } from "@angular/material/radio";
 import { CashFlowService } from "../../../services/cash-flow.service";
-import { CashFlowDto } from "../../../models/cashflow";
+import { CashFlowDto, FLOW_TYPES } from "../../../models/cashflow";
+import { CATEGORY_TYPES } from "../../../models/expense";
 
 @Component({
 	selector: "app-add-cash-flow-panel",
@@ -36,17 +42,26 @@ import { CashFlowDto } from "../../../models/cashflow";
 		FormsModule,
 		MatTabsModule,
 		MatRadioModule,
+		ReactiveFormsModule,
 	],
 	templateUrl: "./add-cash-flow-panel.component.html",
 	styleUrl: "./add-cash-flow-panel.component.css",
-	outputs: [],
 })
 export class AddCashFlowPanelComponent {
 	title = "add-cashflow";
 	submitted = false;
 	cashflowService = inject(CashFlowService);
-	newCashflow = new CashFlowDto(1, "", "", 0, 0, 0);
+
 	cashFlows: CashFlowDto[] = [];
+
+	newCashFlowForm = new FormGroup({
+		id: new FormControl(0),
+		name: new FormControl(""),
+		description: new FormControl(""),
+		amount: new FormControl(0),
+		flow: new FormControl(FLOW_TYPES.income),
+		category: new FormControl(CATEGORY_TYPES.None),
+	});
 
 	updateTable(): void {
 		this.cashflowService.GetAllFlows().subscribe((cf) => {
@@ -55,7 +70,18 @@ export class AddCashFlowPanelComponent {
 	}
 
 	handleSubmit() {
-		this.cashflowService.CreateFlow(this.newCashflow).subscribe({
+		console.log(this.newCashFlowForm.value.id);
+
+		const newCF = new CashFlowDto(
+			this.newCashFlowForm.value.id ?? 0,
+			this.newCashFlowForm.value.name ?? "",
+			this.newCashFlowForm.value.description ?? "",
+			this.newCashFlowForm.value.amount ?? 0,
+			this.newCashFlowForm.value.flow ?? FLOW_TYPES.income,
+			this.newCashFlowForm.value.category ?? CATEGORY_TYPES.None,
+		);
+
+		this.cashflowService.CreateFlow(newCF).subscribe({
 			complete: () => {
 				this.submitted = true;
 				this.updateTable();
---
+++ PennyPincherUI/src/app/cash-flows/cash-flow-forms/edit-cash-flow-panel/edit-cash-flow-panel.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-cash-flow-panel',
+  imports: [],
+  templateUrl: './edit-cash-flow-panel.component.html',
+  styleUrl: './edit-cash-flow-panel.component.css'
+})
+export class EditCashFlowPanelComponent {
+
+}
