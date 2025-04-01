b50d9cf6be26c5011f45cf0d5883b039d4515b23
Joseph-Ramon-Lucas <joseph.ramon.lucas@gmail.com>
2025-04-01 17:15:55 -0400

    Updated Analysis endpoints with custom Dto types and created a component

---
+++ PennyPincher.API/PennyPincher/Models/AnalysisComparisonDto.cs
@@ -0,0 +1,15 @@
+﻿namespace PennyPincher.Models
+{
+    public class AnalysisComparisonDto
+    {
+        public IEnumerable<CashFlowDto> CurrentTopExpenses { get; set; } = Enumerable.Empty<CashFlowDto>();
+        public IEnumerable<CashFlowDto> ProjectedTopExpenses { get; set; } = Enumerable.Empty<CashFlowDto>();
+        public IEnumerable<CashFlowDto> CurrentCategorySum { get; set; } = Enumerable.Empty<CashFlowDto>();
+        public IEnumerable<CashFlowDto> ProjectedCategorySum { get; set; } = Enumerable.Empty<CashFlowDto>();
+        public double CurrentMostCostlyCategoryAmount { get; set; }
+        public double ProjectedMostCostlyCategoryAmount { get; set; }
+        public CategoryTypes CurrentMostCostlyCategory { get; set; }
+        public CategoryTypes ProjectedMostCostlyCategory { get; set; }
+        public double CostlyCategoryRatio { get; set; }
+    }
+}
--- PennyPincherUI/src/app/models/analysis.ts
+++ PennyPincherUI/src/app/models/analysis.ts
@@ -1,4 +1,33 @@
+import type { CashFlowDto } from "./cashflow";
+import type { CATEGORY_TYPES } from "./expense";
+
 export enum CFDataStores {
 	Current = 0,
 	Projected = 1,
+}
+
+export class AnalysisStatusDto {
+	constructor(
+		public GrossIncome: number,
+		public NetIncome: number,
+		public Liabilities: number,
+		public NetIncomeRatio: number,
+		public MostCostlyName: string,
+		public MostCostlyAmount: number,
+		public PercentOfEarningsGoingToMostCostlyAmount: number,
+	) {}
+}
+
+export class AnalysisComparisonDto {
+	constructor(
+		public CurrentTopExpenses: CashFlowDto[],
+		public ProjectedTopExpenses: CashFlowDto[],
+		public CurrentCategorySum: CashFlowDto[],
+		public ProjectedCategorySum: CashFlowDto[],
+		public CurrentMostCostlyCategoryAmount: number,
+		public ProjectedMostCostlyCategoryAmount: number,
+		public CurrentMostCostlyCategory: CATEGORY_TYPES,
+		public ProjectedMostCostlyCategory: CATEGORY_TYPES,
+		public CostlyCategoryRatio: number,
+	) {}
 }
